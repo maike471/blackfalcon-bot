469d9496a7f923a2cb7b39b6f667672080b6cfa5
maike471 <moftahalmshre325@gmail.com>
2026-07-23 00:32:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8740964769:AAGY9AtqUzXwKyPHLjYCyF83wiIe3u3bD_o"
+TOKEN = "8740964769:AAEeF2xJvZ1zKELxixAy4t3evkZ5Om_cqMA"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("أهلاً بك! البوت يعمل بنجاح. 🎉")
